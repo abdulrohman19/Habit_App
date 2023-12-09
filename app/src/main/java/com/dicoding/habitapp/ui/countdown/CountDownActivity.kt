a528ab0b6da0461989d79a6c2f3c92b998bc81b5
abdul rohman <abdulrohmanunida@gmail.com>
2023-12-09 17:38:12 +0700

    ToDo - 10,13

--- app/src/main/java/com/dicoding/habitapp/ui/countdown/CountDownActivity.kt
+++ app/src/main/java/com/dicoding/habitapp/ui/countdown/CountDownActivity.kt
@@ -6,15 +6,26 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.IntentCompat.getParcelableExtra
 import androidx.lifecycle.ViewModelProvider
+import androidx.work.Data
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
 import com.dicoding.habitapp.R
 import com.dicoding.habitapp.data.Habit
+import com.dicoding.habitapp.databinding.ActivityCountDownBinding
+import com.dicoding.habitapp.notification.NotificationWorker
 import com.dicoding.habitapp.utils.HABIT
+import com.dicoding.habitapp.utils.HABIT_ID
+import com.dicoding.habitapp.utils.HABIT_TITLE
+import java.util.concurrent.TimeUnit
 
 class CountDownActivity : AppCompatActivity() {
+    private var _binding: ActivityCountDownBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_count_down)
+        _binding = ActivityCountDownBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportActionBar?.title = "Count Down"
 
         val habit = getParcelableExtra(intent, HABIT, Habit::class.java)
@@ -22,18 +33,33 @@ class CountDownActivity : AppCompatActivity() {
         if (habit != null){
             findViewById<TextView>(R.id.tv_count_down_title).text = habit.title
 
-            val viewModel = ViewModelProvider(this).get(CountDownViewModel::class.java)
+            val viewModel = ViewModelProvider(this)[CountDownViewModel::class.java]
 
             //TODO 10 : Set initial time and observe current time. Update button state when countdown is finished
+            viewModel.setInitialTime(habit.minutesFocus)
+            viewModel.currentTimeString.observe(this){time ->
+                binding.tvCountDown.text = time
+            }
 
             //TODO 13 : Start and cancel One Time Request WorkManager to notify when time is up.
+            val workManager = WorkManager.getInstance(this)
+            val data = Data.Builder().putString(HABIT_TITLE, habit.title).putInt(HABIT_ID, habit.id)
+                .build()
+            val oneTimeWorkRequest = OneTimeWorkRequest.Builder(NotificationWorker::class.java)
+                .setInputData(data)
+                .setInitialDelay(habit.minutesFocus, TimeUnit.MINUTES)
+                .build()
 
             findViewById<Button>(R.id.btn_start).setOnClickListener {
-
+                workManager.enqueue(oneTimeWorkRequest)
+                viewModel.startTimer()
+                updateButtonState(true)
             }
 
             findViewById<Button>(R.id.btn_stop).setOnClickListener {
-
+                workManager.cancelWorkById(oneTimeWorkRequest.id)
+                viewModel.resetTimer()
+                updateButtonState(false)
             }
         }
 
