9c02b98fb7c3171fd9d192103656162115c08b5d
abdul rohman <abdulrohmanunida@gmail.com>
2023-12-09 17:29:03 +0700

    ToDo - 8,9

--- app/src/main/java/com/dicoding/habitapp/ui/list/HabitAdapter.kt
+++ app/src/main/java/com/dicoding/habitapp/ui/list/HabitAdapter.kt
@@ -1,5 +1,6 @@
 package com.dicoding.habitapp.ui.list
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -16,11 +17,14 @@ class HabitAdapter(
 
     //TODO 8 : Create and initialize ViewHolder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HabitViewHolder {
-        throw NotImplementedError("Not yet implemented")
+        val itemHabit = LayoutInflater.from(parent.context).inflate(R.layout.habit_item,parent,false)
+        return HabitViewHolder(itemHabit)
     }
 
     override fun onBindViewHolder(holder: HabitViewHolder, position: Int) {
         //TODO 9 : Get data and bind them to ViewHolder
+        val habit = getItem(position) as Habit
+        holder.bind(habit)
     }
 
     inner class HabitViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -36,6 +40,12 @@ class HabitAdapter(
             tvTitle.text = habit.title
             tvStartTime.text = habit.startTime
             tvMinutes.text = habit.minutesFocus.toString()
+
+            when (getHabit.priorityLevel.uppercase()) {
+                "HIGH" -> R.drawable.ic_priority_high
+                "MEDIUM" -> R.drawable.ic_priority_medium
+                else -> R.drawable.ic_priority_low
+            }.let { ivPriority.setImageResource(it) }
             itemView.setOnClickListener {
                 onClick(habit)
             }
